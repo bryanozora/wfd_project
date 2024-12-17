4cdc50127ceae8162649bb70140dcbc938b736c1
bryanozora <bryan19rnr@gmail.com>
2024-12-17 10:17:28 +0700

    fix7

--- app/Http/Controllers/userController.php
+++ app/Http/Controllers/userController.php
@@ -1,8 +1,8 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers; 
 
-use App\Models\User;
+use App\Models\user;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
 
